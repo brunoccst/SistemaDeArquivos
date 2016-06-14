1faee804a3f31ef658dd300c7e8d2a5f9e2f94ae
MATHEUS SILVA DOS SANTOS <14104987@l1975028.portoalegre.pucrsnet.br>
2016-06-13 21:22:44 -0300

    commit

--- FileManager.c
+++ FileManager.c
@@ -16,16 +16,19 @@ struct rootdir{
 	unsigned char reserved[12];
 };
 
-const char * init_ = "-init";
-const char * create_ = "-create";
-const char * read_ = "-read";
-const char * del_ = "-del";
-const char * ls_ = "-ls";
+struct rootdir ROOT;
+
+
+const char * init_ = "-init\0";
+const char * create_ = "-create\0";
+const char * read_ = "-read\0";
+const char * del_ = "-del\0";
+const char * ls_ = "-ls\0";
 
 unsigned char sector_data[1024];
 unsigned short int sector_index[512];
-char* consoleCommand;
-char* fileName;
+char consoleCommand[10];
+char fileName[20];
 
 int isEOF(struct rootdir_entry * entry){
 	return ( * entry ).size == 0
@@ -38,47 +41,70 @@ int isDeleted(struct rootdir_entry * entry){
 				&& !( * entry ).name[0] == 0 ;
 };
 
+
+
+void startRoot(){
+	memset(&ROOT,0,sizeof(struct rootdir));
+	ROOT.free_blocks = 4096; // PRIMEIRO INDEX
+}
+void writeRoot(FILE * file){
+	fwrite(&ROOT,sizeof(struct rootdir),1, file);
+}
+void readRoot(FILE * file){
+	fread(&ROOT,sizeof(struct rootdir),1, file);
+}
+
 void init()
 {
+	//printf("'teste record ---> ROOT.free_blocks = %u\n'",ROOT.free_blocks);
+	startRoot();
+	remove("simulfs");
+	//printf("'teste record ---> ROOT.free_blocks = %u\n'",ROOT.free_blocks);
 	FILE *fptr;
-	if ( !( fptr = fopen( "simul.fs", "wb" ))){
+	if ( !( fptr = fopen( "simul.fs", "wb+" ))){
 	    printf( "File could not be opened.\n" );
 	}
 	else
 	{
-	    printf("Initializing 'simul.fs'");
-	    char * buff = (char*)malloc(MAX_SIZE);
-	    memset(buff, 0, MAX_SIZE);
-	    fwrite (buff , sizeof(char), MAX_SIZE, fptr);
-	    printf("'simul.fs' initialized.");
+	    printf("Initializing 'simul.fs'\n");
+		writeRoot(fptr);
+		fseek(fptr,0,SEEK_SET);
+	    printf("'simul.fs' initialized.\n");
+		ROOT.free_blocks = 0;
+		readRoot(fptr);
+		//printf("'teste record ---> ROOT.free_blocks = %u'\n",ROOT.free_blocks);
 	}
 }
 
-void create(char name[] ){}
+void create(char name[] ){
+
+
+}
 void read(char name[]){}
 void delete(char name[]){}
 void deleteFile(FILE * file){}
 void list(){}
 
 void main(int argc, const char* argv[]){
-
-	consoleCommand = (char*) argv[1];
-	fileName = (char*) argv[2];
-
+	memset(&fileName,0,20);
+	sprintf(consoleCommand,"%s\0",(char*) argv[1]);
 	if (strcmp(consoleCommand, init_) == 0)
 	{
 		init();
 	}
 	else if (strcmp(consoleCommand, create_) == 0)
 	{
+		sprintf(fileName,"%s\0",(char*) argv[2]);
 		create(fileName);
 	}
 	else if (strcmp(consoleCommand, read_) == 0)
 	{
+		sprintf(fileName,"%s\0",(char*) argv[2]);
 		read(fileName);
 	}
 	else if (strcmp(consoleCommand, del_) == 0)
 	{
+		sprintf(fileName,"%s\0",(char*) argv[2]);
 		delete(fileName);
 	}
 	else if (strcmp(consoleCommand, ls_) == 0)
